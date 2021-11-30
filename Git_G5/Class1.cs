1643099dde796aa83bbae2b13b81c349f8e75acd
Andrii Bykov <Andrii.Bykov@infopulse.com>
2021-11-30 21:06:49 +0200

    test

--- Git_G5/Class1.cs
+++ Git_G5/Class1.cs
@@ -4,7 +4,7 @@
     {
         public int firstTest = 5;
         public int secondTest = 55;
-        public int thirdTest = 0;
+        public int thirdTest = 0000;
         public int fourthTest = 1;
         public int fifthTest = 23;
     }
